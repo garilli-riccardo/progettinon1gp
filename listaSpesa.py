bd685f9e128ae57f591e28d7f02098387a8cefc4
Riccardo Garilli <garilli.riccardo@iisgalvanimi.edu.it>
2024-11-20 10:00:39 +0000

    sistemato qualcosa ma va terminato

--- listaSpesa.py
+++ listaSpesa.py
@@ -1,10 +1,36 @@
 lista = []
-def aggiungiElementi(x, lista):
+
+def aggiungiElementi():
+    x = input()
     lista.append(x) # l'input va fatto nel main
 
 def visualizzaElementi(lista):
     for i in range(len(lista)):
         print(f"{i + 1}. {lista[i]}") #
 
-def rimuoviElementi(lista):
+def rimuoviElementi():
+    x = input()
     lista.pop(x)
+
+def contaElementi():
+
+    print(f"Numero di elementi nella lista: {len(lista)}")
+
+def svuotaLista():
+    lista.clear()
+
+while True:
+    print("premi 0 per uscire,\npremi 1 per aggiungerre un elemento,\npremi 2 per visualizzare la lista,\n premi 3 per eliminare un elemento,\n premi 4 per contare gli elementi della lista,\n premi 5 per eliminare un elemento")
+    x=int(input(""))
+    if x == 0:
+        break
+    elif x == 1:
+        aggiungiElementi()
+    elif x == 2:
+        visualizzaElementi()
+    elif x == 3:
+        rimuoviElementi()
+    elif x == 4:
+        contaElementi()
+    elif x == 5:
+        svuotaLista()
