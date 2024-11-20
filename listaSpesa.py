121838fbdbb205de80646e107ef5662afb67351f
Riccardo Garilli <garilli.riccardo@iisgalvanimi.edu.it>
2024-11-20 09:36:46 +0000

    visualizza elementi

--- listaSpesa.py
+++ listaSpesa.py
@@ -2,4 +2,7 @@ lista = []
 def aggiungiElementi(x, lista):
     lista.append(x) # l'input va fatto nel main
 
+def visualizzaElementi(lista):
+    for i in range(len(lista)):
+        print(f"{i + 1}. {lista[i]}") #
 
