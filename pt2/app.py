dbf913c216cce865293f192df92ab09e40b7f7e4
Riccardo Garilli <garilli.riccardo@iisgalvanimi.edu.it>
2024-11-27 10:09:45 +0000

    quasi finito

--- pt2/app.py
+++ pt2/app.py
@@ -1,15 +1,30 @@
-
-
 from flask import Flask 
  
-#inizializza l'app Flask 
+
 app = Flask(__name__) 
- 
-#rotta principale 
+lista_spesa = []
+
 @app.route('/') 
 def home(): 
  return "Per ora funziona tutto" 
- 
+
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    x = input()
+    lista_spesa.append(x)
+    return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista_spesa):
+    lista_spesa.pop(indice - 1)
+    return redirect(url_for('home'))
+
+@app.route('/svuotalista', methods=['POST'])
+def svuotalista():
+    lista_spesa.clear()
+    return redirect(url_for('home'))
+
 #avvio dell'app Flask 
 if __name__ == '__main__': 
- app.run(debug=True)
+    app.run(debug=True)
